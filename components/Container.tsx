8afc68b60a8550b4417565f220be64c623de263a
Anwin Joselyn <anwin.joselyn@clickpost.in>
2021-08-09 20:41:01 +0530

    added basic sidebar

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,10 +1,16 @@
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container">
-      <Header />
-      {children}
+    <div className="flex">
+      <div className="w-1/6  h-screen">
+        <Sidebar />
+      </div>
+      <div className="w-5/6 ...">
+        <Header />
+        {children}
+      </div>
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,19 +5,26 @@ export default function Header() {
   const auth = useRequireAuth();
 
   return (
-    <div className="py-5 bg-soft-red flex flex-row justify-between content-center">
+    <div className="py-3 bg-orange flex flex-row justify-between content-center items-center">
       <Link href="/" passHref>
-        <h1 className="primary-text-color hover:text-gray-200 ml-7 font-bold mb-1 cursor-pointer">
-          To Dos
+        <h1 className="text-primary-text-color hover:text-secondary-text-color ml-7 font-bold mb-1">
+          To Do App
         </h1>
       </Link>
-      {auth.user ? (
-        <Link href="/profile" passHref>
-          <span className="material-icons hover:text-gray-200 text-3xl lg:text-4xl mr-7 self-center cursor-pointer">
-            account_circle
+      <div className="flex items-center">
+        <Link href="/dashboard" passHref>
+          <span className="hover:text-secondary-text-color mr-7 self-center cursor-pointer">
+            Dashboard
           </span>
         </Link>
-      ) : null}
+        {auth.user ? (
+          <Link href="/profile" passHref>
+            <span className="material-icons hover:text-secondary-text-color text-3xl lg:text-4xl mr-7 self-center cursor-pointer">
+              account_circle
+            </span>
+          </Link>
+        ) : null}
+      </div>
     </div>
   );
 }
