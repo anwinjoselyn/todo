5c665e61ae1e53003c6539ac19816dd72fc5c69e
Anwin Joselyn <anwin.joselyn@clickpost.in>
2021-08-21 17:32:58 +0530

    added purge details

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: { enabled: true, content: ['./src/**/*.html', './src/**/*.jsx'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
