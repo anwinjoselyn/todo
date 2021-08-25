3ec6f4ecc734d8a35b02bedf94f19107ebb3c912
Anwin Joselyn <anwin.joselyn@clickpost.in>
2021-08-25 20:21:49 +0530

    removed auth from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,15 @@ import '../styles/style.css';
 
 import { AppProps } from 'next/app';
 
-import useRequireAuth from '../hooks/useRequireAuth';
-
 import Container from '../components/Container';
 
 import { AuthProvider } from '../hooks/useAuth';
 
 function MyApp({ Component, pageProps }: AppProps): any {
-  const auth = useRequireAuth();
-  console.log('auth', auth);
-
     return (
       <AuthProvider>
         <Container>
-          <Component {...pageProps} appAuth={auth} />
+          <Component {...pageProps} />
         </Container>
       </AuthProvider>
     );
